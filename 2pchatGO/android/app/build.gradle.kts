d985d2eb590c8bcab23b3f2c056a6c66ae914741
kodzy <162982427+kodzyfox@users.noreply.github.com>
2026-09-02 00:06:43 +0400

    release: publish version v0.0.8.7

--- 2pchatGO/android/app/build.gradle.kts
+++ 2pchatGO/android/app/build.gradle.kts
@@ -91,8 +91,8 @@ android {
         applicationId = "com.example.twopchat.go"
         minSdk = 24
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.0.8.6"
+        versionCode = 20
+        versionName = "0.0.8.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters.addAll(listOf("arm64-v8a", "x86_64"))
